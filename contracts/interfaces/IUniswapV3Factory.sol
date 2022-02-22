2a97c10a127d3faa4b45dbfe1bceaf8cd5dee022
Aoi <ht.so.ik0102@gmail.com>
2022-02-22 19:34:05 +0700

    continue to interface

---
+++ contracts/interfaces/IUniswapV3Factory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.0;
+
+interface IUniswapV3Factory {
+    event OwnerChaned(address indexed oldOwner, address indexed newOwner);
+
+    event PoolCreated(
+        address indexed token0,
+        address indexed token1,
+        uint24 indexed fee,
+        int24 tickSpacing,
+        address pool
+    );
+
+    event FeeAmountEnabled(uint24 indexed fee, int24 indexed tickSpacing);
+
+    function owner() external view returns (address);
+
+    function feeAmountTickSpacing(uint24 fee) external view returns (int24);
+
+    function getPool(
+        address tokenA,
+        address tokenB,
+        uint24 fee
+    ) external view returns (address pool);
+}
