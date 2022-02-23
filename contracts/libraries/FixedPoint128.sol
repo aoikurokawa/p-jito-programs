de8722256620d303f175e6b7cb398f60a2e4aa7a
Aoi <ht.so.ik0102@gmail.com>
2022-02-23 22:15:42 +0700

    fixedpoint128

---
+++ contracts/libraries/FixedPoint128.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// A library for handling binary fixed point numbers, 
+library FixedPoint128 {
+    uint256 internal constant Q128 = 0x100000000000000000000000000000000;
+}
