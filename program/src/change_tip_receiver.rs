8df4f6a90e3341606ea886750136e74ca3280383
aoikurokawa <ht.so.ik0102@gmail.com>
2025-07-21 08:52:53 +0900

    feat: update

--- program/src/change_tip_receiver.rs
+++ program/src/change_tip_receiver.rs
@@ -2,162 +2,221 @@ use jito_tip_payment_core::{config::Config, load_mut_unchecked};
 use pinocchio::{
     account_info::AccountInfo,
     program_error::ProgramError,
+    pubkey::Pubkey,
     sysvars::{rent::Rent, Sysvar},
     ProgramResult,
 };
 
 use crate::handle_payments;
 
-pub struct ChangeTipReceiverAccounts<'a> {
-    /// Config
-    pub config: &'a AccountInfo,
+/// Validator should invoke this instruction before executing any transactions that contain tips.
+/// Validator should also ensure it calls it if there's a fork detected.
+pub fn process_change_tip_receiver(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+) -> Result<(), ProgramError> {
+    let [config, old_tip_receiver, new_tip_receiver, block_builder, tip_payment_account_0, tip_payment_account_1, tip_payment_account_2, tip_payment_account_3, tip_payment_account_4, tip_payment_account_5, tip_payment_account_6, tip_payment_account_7, signer] =
+        accounts
+    else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
 
-    /// Old tip receiver
-    pub old_tip_receiver: &'a AccountInfo,
+    let rent = Rent::get()?;
 
-    /// New tip receiver
-    pub new_tip_receiver: &'a AccountInfo,
+    // Create PDA signer seeds
+    // let seeds = [
+    //     Seed::from(b"vault"),
+    //     Seed::from(self.accounts.owner.key().as_ref()),
+    //     Seed::from(&self.accounts.bumps),
+    // ];
+    // let signers = [Signer::from(&seeds)];
 
-    /// Block builder
-    pub block_builder: &'a AccountInfo,
+    // Transfer all lamports from vault to owner
+    // Transfer {
+    //     from: self.accounts.vault,
+    //     to: self.accounts.owner,
+    //     lamports: self.accounts.vault.lamports(),
+    // }
+    // .invoke_signed(&signers)?;
 
-    /// Tip payment account 0
-    pub tip_payment_account_0: &'a AccountInfo,
+    let tip_accounts = &[
+        tip_payment_account_0,
+        tip_payment_account_1,
+        tip_payment_account_2,
+        tip_payment_account_3,
+        tip_payment_account_4,
+        tip_payment_account_5,
+        tip_payment_account_6,
+        tip_payment_account_7,
+    ];
 
-    /// Tip payment account 1
-    pub tip_payment_account_1: &'a AccountInfo,
+    let config = unsafe { load_mut_unchecked::<Config>(config.borrow_mut_data_unchecked())? };
 
-    /// Tip payment account 2
-    pub tip_payment_account_2: &'a AccountInfo,
-
-    /// Tip payment account 3
-    pub tip_payment_account_3: &'a AccountInfo,
-
-    /// Tip payment account 4
-    pub tip_payment_account_4: &'a AccountInfo,
-
-    /// Tip payment account 5
-    pub tip_payment_account_5: &'a AccountInfo,
-
-    /// Tip payment account 6
-    pub tip_payment_account_6: &'a AccountInfo,
-
-    /// Tip payment account 7
-    pub tip_payment_account_7: &'a AccountInfo,
-
-    /// Signer
-    pub signer: &'a AccountInfo,
-}
-
-impl<'a> TryFrom<&'a [AccountInfo]> for ChangeTipReceiverAccounts<'a> {
-    type Error = ProgramError;
-
-    fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
-        let [config, old_tip_receiver, new_tip_receiver, block_builder, tip_payment_account_0, tip_payment_account_1, tip_payment_account_2, tip_payment_account_3, tip_payment_account_4, tip_payment_account_5, tip_payment_account_6, tip_payment_account_7, signer] =
-            accounts
-        else {
-            return Err(ProgramError::NotEnoughAccountKeys);
-        };
-
-        // Basic Accounts Checks
-        // if !owner.is_signer() {
-        //     return Err(ProgramError::InvalidAccountOwner);
-        // }
-
-        // if !vault.is_owned_by(&pinocchio_system::ID) {
-        //     return Err(ProgramError::InvalidAccountOwner);
-        // }
-
-        // if vault.lamports().eq(&0) {
-        //     return Err(ProgramError::InvalidAccountData);
-        // }
-
-        // let (vault_key, bump) = find_program_address(&[b"vault", owner.key().as_ref()], &crate::ID);
-        // if &vault_key != vault.key() {
-        //     return Err(ProgramError::InvalidAccountOwner);
-        // }
-
-        Ok(Self {
-            config,
-            old_tip_receiver,
-            new_tip_receiver,
-            block_builder,
-            tip_payment_account_0,
-            tip_payment_account_1,
-            tip_payment_account_2,
-            tip_payment_account_3,
-            tip_payment_account_4,
-            tip_payment_account_5,
-            tip_payment_account_6,
-            tip_payment_account_7,
-            signer,
-        })
+    unsafe {
+        handle_payments(
+            &rent,
+            tip_accounts,
+            &old_tip_receiver,
+            &block_builder,
+            config.block_builder_commission_pct,
+        )?;
     }
+
+    config.tip_receiver = *new_tip_receiver.key();
+
+    Ok(())
 }
 
-pub struct ChangeTipReceiver<'a> {
-    pub accounts: ChangeTipReceiverAccounts<'a>,
-}
-
-impl<'a> TryFrom<&'a [AccountInfo]> for ChangeTipReceiver<'a> {
-    type Error = ProgramError;
-
-    fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
-        let accounts = ChangeTipReceiverAccounts::try_from(accounts)?;
-
-        Ok(Self { accounts })
-    }
-}
-
-impl<'a> ChangeTipReceiver<'a> {
-    pub const DISCRIMINATOR: &'a u8 = &1;
-
-    pub fn process(&mut self) -> ProgramResult {
-        let rent = Rent::get()?;
-
-        // Create PDA signer seeds
-        // let seeds = [
-        //     Seed::from(b"vault"),
-        //     Seed::from(self.accounts.owner.key().as_ref()),
-        //     Seed::from(&self.accounts.bumps),
-        // ];
-        // let signers = [Signer::from(&seeds)];
-
-        // Transfer all lamports from vault to owner
-        // Transfer {
-        //     from: self.accounts.vault,
-        //     to: self.accounts.owner,
-        //     lamports: self.accounts.vault.lamports(),
-        // }
-        // .invoke_signed(&signers)?;
-
-        let tip_accounts = &[
-            self.accounts.tip_payment_account_0,
-            self.accounts.tip_payment_account_1,
-            self.accounts.tip_payment_account_2,
-            self.accounts.tip_payment_account_3,
-            self.accounts.tip_payment_account_4,
-            self.accounts.tip_payment_account_5,
-            self.accounts.tip_payment_account_6,
-            self.accounts.tip_payment_account_7,
-        ];
-
-        let config = unsafe {
-            load_mut_unchecked::<Config>(self.accounts.config.borrow_mut_data_unchecked())?
-        };
-
-        unsafe {
-            handle_payments(
-                &rent,
-                tip_accounts,
-                &self.accounts.old_tip_receiver,
-                &self.accounts.block_builder,
-                config.block_builder_commission_pct,
-            )?;
-        }
-
-        config.tip_receiver = *self.accounts.new_tip_receiver.key();
-
-        Ok(())
-    }
-}
+// pub struct ChangeTipReceiverAccounts<'a> {
+//     /// Config
+//     pub config: &'a AccountInfo,
+//
+//     /// Old tip receiver
+//     pub old_tip_receiver: &'a AccountInfo,
+//
+//     /// New tip receiver
+//     pub new_tip_receiver: &'a AccountInfo,
+//
+//     /// Block builder
+//     pub block_builder: &'a AccountInfo,
+//
+//     /// Tip payment account 0
+//     pub tip_payment_account_0: &'a AccountInfo,
+//
+//     /// Tip payment account 1
+//     pub tip_payment_account_1: &'a AccountInfo,
+//
+//     /// Tip payment account 2
+//     pub tip_payment_account_2: &'a AccountInfo,
+//
+//     /// Tip payment account 3
+//     pub tip_payment_account_3: &'a AccountInfo,
+//
+//     /// Tip payment account 4
+//     pub tip_payment_account_4: &'a AccountInfo,
+//
+//     /// Tip payment account 5
+//     pub tip_payment_account_5: &'a AccountInfo,
+//
+//     /// Tip payment account 6
+//     pub tip_payment_account_6: &'a AccountInfo,
+//
+//     /// Tip payment account 7
+//     pub tip_payment_account_7: &'a AccountInfo,
+//
+//     /// Signer
+//     pub signer: &'a AccountInfo,
+// }
+//
+// impl<'a> TryFrom<&'a [AccountInfo]> for ChangeTipReceiverAccounts<'a> {
+//     type Error = ProgramError;
+//
+//     fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
+//         let [config, old_tip_receiver, new_tip_receiver, block_builder, tip_payment_account_0, tip_payment_account_1, tip_payment_account_2, tip_payment_account_3, tip_payment_account_4, tip_payment_account_5, tip_payment_account_6, tip_payment_account_7, signer] =
+//             accounts
+//         else {
+//             return Err(ProgramError::NotEnoughAccountKeys);
+//         };
+//
+//         // Basic Accounts Checks
+//         // if !owner.is_signer() {
+//         //     return Err(ProgramError::InvalidAccountOwner);
+//         // }
+//
+//         // if !vault.is_owned_by(&pinocchio_system::ID) {
+//         //     return Err(ProgramError::InvalidAccountOwner);
+//         // }
+//
+//         // if vault.lamports().eq(&0) {
+//         //     return Err(ProgramError::InvalidAccountData);
+//         // }
+//
+//         // let (vault_key, bump) = find_program_address(&[b"vault", owner.key().as_ref()], &crate::ID);
+//         // if &vault_key != vault.key() {
+//         //     return Err(ProgramError::InvalidAccountOwner);
+//         // }
+//
+//         Ok(Self {
+//             config,
+//             old_tip_receiver,
+//             new_tip_receiver,
+//             block_builder,
+//             tip_payment_account_0,
+//             tip_payment_account_1,
+//             tip_payment_account_2,
+//             tip_payment_account_3,
+//             tip_payment_account_4,
+//             tip_payment_account_5,
+//             tip_payment_account_6,
+//             tip_payment_account_7,
+//             signer,
+//         })
+//     }
+// }
+//
+// pub struct ChangeTipReceiver<'a> {
+//     pub accounts: ChangeTipReceiverAccounts<'a>,
+// }
+//
+// impl<'a> TryFrom<&'a [AccountInfo]> for ChangeTipReceiver<'a> {
+//     type Error = ProgramError;
+//
+//     fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
+//         let accounts = ChangeTipReceiverAccounts::try_from(accounts)?;
+//
+//         Ok(Self { accounts })
+//     }
+// }
+//
+// impl<'a> ChangeTipReceiver<'a> {
+//     pub const DISCRIMINATOR: &'a u8 = &1;
+//
+//     pub fn process(&mut self) -> ProgramResult {
+//         let rent = Rent::get()?;
+//
+//         // Create PDA signer seeds
+//         // let seeds = [
+//         //     Seed::from(b"vault"),
+//         //     Seed::from(self.accounts.owner.key().as_ref()),
+//         //     Seed::from(&self.accounts.bumps),
+//         // ];
+//         // let signers = [Signer::from(&seeds)];
+//
+//         // Transfer all lamports from vault to owner
+//         // Transfer {
+//         //     from: self.accounts.vault,
+//         //     to: self.accounts.owner,
+//         //     lamports: self.accounts.vault.lamports(),
+//         // }
+//         // .invoke_signed(&signers)?;
+//
+//         let tip_accounts = &[
+//             self.accounts.tip_payment_account_0,
+//             self.accounts.tip_payment_account_1,
+//             self.accounts.tip_payment_account_2,
+//             self.accounts.tip_payment_account_3,
+//             self.accounts.tip_payment_account_4,
+//             self.accounts.tip_payment_account_5,
+//             self.accounts.tip_payment_account_6,
+//             self.accounts.tip_payment_account_7,
+//         ];
+//
+//         let config = unsafe {
+//             load_mut_unchecked::<Config>(self.accounts.config.borrow_mut_data_unchecked())?
+//         };
+//
+//         unsafe {
+//             handle_payments(
+//                 &rent,
+//                 tip_accounts,
+//                 &self.accounts.old_tip_receiver,
+//                 &self.accounts.block_builder,
+//                 config.block_builder_commission_pct,
+//             )?;
+//         }
+//
+//         config.tip_receiver = *self.accounts.new_tip_receiver.key();
+//
+//         Ok(())
+//     }
+// }
--- sdk/src/instruction.rs
+++ sdk/src/instruction.rs
@@ -34,12 +34,25 @@ pub enum JitoTipPaymentInstruction {
     ChangeTipReceiver,
 
     /// Change block builder
-    ChangeBlockBuilder,
+    #[account(0, writable, name = "config")]
+    #[account(1, writable, name = "tip_receiver")]
+    #[account(2, writable, name = "old_block_builder")]
+    #[account(3, writable, name = "new_block_builder")]
+    #[account(4, writable, name = "tip_payment_account_0")]
+    #[account(5, writable, name = "tip_payment_account_1")]
+    #[account(6, writable, name = "tip_payment_account_2")]
+    #[account(7, writable, name = "tip_payment_account_3")]
+    #[account(8, writable, name = "tip_payment_account_4")]
+    #[account(9, writable, name = "tip_payment_account_5")]
+    #[account(10, writable, name = "tip_payment_account_6")]
+    #[account(11, writable, name = "tip_payment_account_7")]
+    #[account(12, writable, signer, name = "signer")]
+    ChangeBlockBuilder { block_builder_commission: u64 },
 }
 
 impl JitoTipPaymentInstruction {
     pub const fn try_from_slice(instruction_data: &[u8]) -> Result<Self, ProgramError> {
-        let [discriminator0, discriminator1, discriminator2, discriminator3, discriminator4, discriminator5, discriminator6, discriminator7] =
+        let [discriminator0, discriminator1, discriminator2, discriminator3, discriminator4, discriminator5, discriminator6, discriminator7, remaining @ ..] =
             instruction_data
         else {
             return Err(ProgramError::InvalidInstructionData);
@@ -62,7 +75,15 @@ impl JitoTipPaymentInstruction {
             [69, 99, 22, 71, 11, 231, 86, 143] => Ok(Self::ChangeTipReceiver),
 
             // ChangeBlockBuilder
-            [134, 80, 38, 137, 165, 21, 114, 123] => Ok(Self::ChangeBlockBuilder),
+            [134, 80, 38, 137, 165, 21, 114, 123] => {
+                let mut slice = [0, 0, 0, 0, 0, 0, 0, 0];
+                slice.copy_from_slice(remaining);
+                let block_builder_commission = u64::from_be_bytes(slice);
+
+                Ok(Self::ChangeBlockBuilder {
+                    block_builder_commission,
+                })
+            }
             _ => Err(ProgramError::InvalidInstructionData),
         }
     }
