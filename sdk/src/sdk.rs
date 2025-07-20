aa17d8069641388ad26fb9668c72ff0d88a7b44c
aoikurokawa <ht.so.ik0102@gmail.com>
2025-07-20 16:39:47 +0900

    fix: update

--- sdk/src/sdk.rs
+++ sdk/src/sdk.rs
@@ -32,6 +32,44 @@ pub fn initialize_config(
     Instruction {
         program_id: *program_id,
         accounts,
-        data: vec![0],
+        data: vec![JitoTipPaymentInstruction::Initialize as u8],
+    }
+}
+
+pub fn change_tip_receiver(
+    program_id: &Pubkey,
+    config: &Pubkey,
+    old_tip_receiver: &Pubkey,
+    new_tip_receiver: &Pubkey,
+    block_builder: &Pubkey,
+    tip_payment_account_0: &Pubkey,
+    tip_payment_account_1: &Pubkey,
+    tip_payment_account_2: &Pubkey,
+    tip_payment_account_3: &Pubkey,
+    tip_payment_account_4: &Pubkey,
+    tip_payment_account_5: &Pubkey,
+    tip_payment_account_6: &Pubkey,
+    tip_payment_account_7: &Pubkey,
+    signer: &Pubkey,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(*config, false),
+        AccountMeta::new(*old_tip_receiver, false),
+        AccountMeta::new(*new_tip_receiver, false),
+        AccountMeta::new(*block_builder, false),
+        AccountMeta::new(*tip_payment_account_0, false),
+        AccountMeta::new(*tip_payment_account_1, false),
+        AccountMeta::new(*tip_payment_account_2, false),
+        AccountMeta::new(*tip_payment_account_3, false),
+        AccountMeta::new(*tip_payment_account_4, false),
+        AccountMeta::new(*tip_payment_account_5, false),
+        AccountMeta::new(*tip_payment_account_6, false),
+        AccountMeta::new(*tip_payment_account_7, false),
+        AccountMeta::new(*signer, true),
+    ];
+    Instruction {
+        program_id: *program_id,
+        accounts,
+        data: vec![JitoTipPaymentInstruction::ChangeTipReceiver as u8],
     }
 }
