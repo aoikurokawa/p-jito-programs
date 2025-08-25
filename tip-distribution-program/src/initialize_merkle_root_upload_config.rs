1976dd786cd6b140c331880fe331f9dccd01226a
aoikurokawa <ht.so.ik0102@gmail.com>
2025-08-25 12:18:05 +0900

    feat: initialize_merkle_root_upload_config

--- tip-distribution-program/src/initialize_merkle_root_upload_config.rs
+++ tip-distribution-program/src/initialize_merkle_root_upload_config.rs
@@ -63,7 +63,9 @@ pub fn process_initialize_merkle_root_upload_config(
     )?;
 
     let merkle_root_upload_config = unsafe {
-        load_mut_unchecked::<MerkleRootUploadConfig>(config_info.borrow_mut_data_unchecked())?
+        load_mut_unchecked::<MerkleRootUploadConfig>(
+            merkle_root_upload_config_info.borrow_mut_data_unchecked(),
+        )?
     };
 
     // Set the bump and override authority
