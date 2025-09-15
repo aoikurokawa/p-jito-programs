b5387b9548200a916b22d12c02ea065274c21be8
aoikurokawa <ht.so.ik0102@gmail.com>
2025-09-15 09:19:09 +0900

    feat: merkle root upload config

--- cli/src/tip_distribution_handler.rs
+++ cli/src/tip_distribution_handler.rs
@@ -1,7 +1,7 @@
 use std::{str::FromStr, sync::Arc};
 
 use anchor_lang::{AccountDeserialize, InstructionData, ToAccountMetas};
-use jito_tip_distribution_legacy::state::{Config, TipDistributionAccount};
+use jito_tip_distribution_legacy::state::{Config, MerkleRootUploadConfig, TipDistributionAccount};
 use jito_tip_distribution_sdk_legacy::{
     derive_tip_distribution_account_address,
     instruction::{update_config_ix, UpdateConfigAccounts, UpdateConfigArgs},
@@ -15,8 +15,15 @@ use solana_signer::Signer;
 use solana_transaction::Transaction;
 
 use crate::tip_distribution::{
-    ConfigActions, TipDistributionAccountActions, TipDistributionCommands,
+    ConfigActions, MerkleRootUploadConfigActions, TipDistributionAccountActions,
+    TipDistributionCommands,
 };
+
+fn derive_merkle_root_upload_config_account_address(
+    tip_distribution_program_id: &Pubkey,
+) -> (Pubkey, u8) {
+    Pubkey::find_program_address(&[MerkleRootUploadConfig::SEED], tip_distribution_program_id)
+}
 
 pub struct TipDistributionCliHandler {
     /// RPC Client
@@ -102,6 +109,15 @@ impl TipDistributionCliHandler {
             ),
             TipDistributionCommands::TipDistributionAccount {
                 action:
+                    TipDistributionAccountActions::UploadMerkleRoot {
+                        vote_account,
+                        root,
+                        max_total_claim,
+                        max_num_nodes,
+                    },
+            } => self.upload_merkle_root(vote_account, root, max_total_claim, max_num_nodes),
+            TipDistributionCommands::TipDistributionAccount {
+                action:
                     TipDistributionAccountActions::Get {
                         vote_account,
                         epoch,
@@ -114,6 +130,19 @@ impl TipDistributionCliHandler {
                         epoch,
                     },
             } => self.close_tip_distribution_account(vote_account, epoch),
+            TipDistributionCommands::MerkleRootUploadConfig {
+                action: MerkleRootUploadConfigActions::Initialize,
+            } => self.initialize_merkle_root_upload_config(),
+            TipDistributionCommands::MerkleRootUploadConfig {
+                action: MerkleRootUploadConfigActions::Update,
+            } => self.update_merkle_root_upload_config(),
+            TipDistributionCommands::MerkleRootUploadConfig {
+                action:
+                    MerkleRootUploadConfigActions::MigrateTdaMerkleRootUploadAuthority {
+                        vote_account,
+                        epoch,
+                    },
+            } => self.migrate_merkle_root_upload_config_authority(vote_account, epoch),
         }
     }
 
@@ -176,6 +205,7 @@ impl TipDistributionCliHandler {
         Ok(())
     }
 
+    /// Update config
     pub fn update_config(
         &self,
         authority: String,
@@ -218,6 +248,110 @@ impl TipDistributionCliHandler {
         Ok(())
     }
 
+    /// Initialize TipDistributionAccount account
+    pub fn initialize_tip_distribution_account(
+        &self,
+        vote_account: Pubkey,
+        merkle_root_upload_authority: Pubkey,
+        validator_commission_bps: u16,
+    ) -> anyhow::Result<()> {
+        let epoch = self.client.get_epoch_info()?.epoch;
+        let (tip_distribution_pubkey, tip_distribution_bump) =
+            derive_tip_distribution_account_address(&self.program_id, &vote_account, epoch);
+
+        let ix = Instruction {
+            program_id: self.program_id,
+            data: jito_tip_distribution_legacy::instruction::InitializeTipDistributionAccount {
+                merkle_root_upload_authority,
+                validator_commission_bps,
+                bump: tip_distribution_bump,
+            }
+            .data(),
+            accounts: jito_tip_distribution_legacy::accounts::InitializeTipDistributionAccount {
+                config: self.config_pda,
+                tip_distribution_account: tip_distribution_pubkey,
+                validator_vote_account: vote_account,
+                signer: self.keypair.pubkey(),
+                system_program: system_program::ID,
+            }
+            .to_account_metas(None),
+        };
+
+        let blockhash = self.client.get_latest_blockhash()?;
+        let tx = Transaction::new_signed_with_payer(
+            &[ix],
+            Some(&self.keypair.pubkey()),
+            &[self.keypair.clone()],
+            blockhash,
+        );
+
+        self.client.send_transaction(&tx)?;
+
+        Ok(())
+    }
+
+    /// Upload merkle root
+    pub fn upload_merkle_root(
+        &self,
+        vote_account: Pubkey,
+        root: String,
+        max_total_claim: u64,
+        max_num_nodes: u64,
+    ) -> anyhow::Result<()> {
+        let root_bytes: Vec<u8> = root
+            .split(',')
+            .map(|byte_str| {
+                byte_str
+                    .trim()
+                    .parse::<u8>()
+                    .map_err(|e| anyhow::anyhow!("Invalid byte '{}': {}", byte_str, e))
+            })
+            .collect::<Result<Vec<u8>, _>>()?;
+
+        if root_bytes.len() != 32 {
+            return Err(anyhow::anyhow!(
+                "Root must be exactly 32 bytes, got {}",
+                root_bytes.len()
+            ));
+        }
+
+        let mut source: [u8; 32] = [0; 32];
+        source.copy_from_slice(&root_bytes);
+
+        let epoch = self.client.get_epoch_info()?.epoch;
+        let (tip_distribution_pubkey, _tip_distribution_bump) =
+            derive_tip_distribution_account_address(&self.program_id, &vote_account, epoch);
+
+        let ix = Instruction {
+            program_id: self.program_id,
+            data: jito_tip_distribution_legacy::instruction::UploadMerkleRoot {
+                root: source,
+                max_total_claim,
+                max_num_nodes,
+            }
+            .data(),
+            accounts: jito_tip_distribution_legacy::accounts::UploadMerkleRoot {
+                config: self.config_pda,
+                merkle_root_upload_authority: self.keypair.pubkey(),
+                tip_distribution_account: tip_distribution_pubkey,
+            }
+            .to_account_metas(None),
+        };
+
+        let blockhash = self.client.get_latest_blockhash()?;
+        let tx = Transaction::new_signed_with_payer(
+            &[ix],
+            Some(&self.keypair.pubkey()),
+            &[self.keypair.clone()],
+            blockhash,
+        );
+
+        self.client.send_transaction(&tx)?;
+
+        Ok(())
+    }
+
+    /// Get TipDistributionAccount account
     pub fn get_tip_distribution_account(
         &self,
         vote_account: String,
@@ -263,47 +397,7 @@ impl TipDistributionCliHandler {
         Ok(())
     }
 
-    pub fn initialize_tip_distribution_account(
-        &self,
-        vote_account: Pubkey,
-        merkle_root_upload_authority: Pubkey,
-        validator_commission_bps: u16,
-    ) -> anyhow::Result<()> {
-        let epoch = self.client.get_epoch_info()?.epoch;
-        let (tip_distribution_pubkey, tip_distribution_bump) =
-            derive_tip_distribution_account_address(&self.program_id, &vote_account, epoch);
-
-        let ix = Instruction {
-            program_id: self.program_id,
-            data: jito_tip_distribution_legacy::instruction::InitializeTipDistributionAccount {
-                merkle_root_upload_authority,
-                validator_commission_bps,
-                bump: tip_distribution_bump,
-            }
-            .data(),
-            accounts: jito_tip_distribution_legacy::accounts::InitializeTipDistributionAccount {
-                config: self.config_pda,
-                tip_distribution_account: tip_distribution_pubkey,
-                validator_vote_account: vote_account,
-                signer: self.keypair.pubkey(),
-                system_program: system_program::ID,
-            }
-            .to_account_metas(None),
-        };
-
-        let blockhash = self.client.get_latest_blockhash()?;
-        let tx = Transaction::new_signed_with_payer(
-            &[ix],
-            Some(&self.keypair.pubkey()),
-            &[self.keypair.clone()],
-            blockhash,
-        );
-
-        self.client.send_transaction(&tx)?;
-
-        Ok(())
-    }
-
+    /// Close TipDistributionAccount account
     pub fn close_tip_distribution_account(
         &self,
         vote_account: Pubkey,
@@ -324,6 +418,107 @@ impl TipDistributionCliHandler {
                 tip_distribution_account: tip_distribution_pda,
                 validator_vote_account: vote_account,
                 signer: self.keypair.pubkey(),
+            }
+            .to_account_metas(None),
+        };
+
+        let blockhash = self.client.get_latest_blockhash()?;
+        let tx = Transaction::new_signed_with_payer(
+            &[ix],
+            Some(&self.keypair.pubkey()),
+            &[self.keypair.clone()],
+            blockhash,
+        );
+
+        self.client.send_transaction(&tx)?;
+
+        Ok(())
+    }
+
+    pub fn initialize_merkle_root_upload_config(&self) -> anyhow::Result<()> {
+        let (merkle_root_upload_upload_config_pda, _merkle_root_upload_upload_config_bump) =
+            derive_merkle_root_upload_config_account_address(&self.program_id);
+
+        let ix = Instruction {
+            program_id: self.program_id,
+            data: jito_tip_distribution_legacy::instruction::InitializeMerkleRootUploadConfig {
+                authority: self.keypair.pubkey(),
+                original_authority: self.keypair.pubkey(),
+            }
+            .data(),
+            accounts: jito_tip_distribution_legacy::accounts::InitializeMerkleRootUploadConfig {
+                config: self.config_pda,
+                merkle_root_upload_config: merkle_root_upload_upload_config_pda,
+                authority: self.keypair.pubkey(),
+                payer: self.keypair.pubkey(),
+                system_program: system_program::ID,
+            }
+            .to_account_metas(None),
+        };
+
+        let blockhash = self.client.get_latest_blockhash()?;
+        let tx = Transaction::new_signed_with_payer(
+            &[ix],
+            Some(&self.keypair.pubkey()),
+            &[self.keypair.clone()],
+            blockhash,
+        );
+
+        self.client.send_transaction(&tx)?;
+
+        Ok(())
+    }
+
+    pub fn update_merkle_root_upload_config(&self) -> anyhow::Result<()> {
+        let (merkle_root_upload_config_pda, _merkle_root_upload_config_bump) =
+            derive_merkle_root_upload_config_account_address(&self.program_id);
+
+        let ix = Instruction {
+            program_id: self.program_id,
+            data: jito_tip_distribution_legacy::instruction::UpdateMerkleRootUploadConfig {
+                authority: self.keypair.pubkey(),
+                original_authority: self.keypair.pubkey(),
+            }
+            .data(),
+            accounts: jito_tip_distribution_legacy::accounts::UpdateMerkleRootUploadConfig {
+                config: self.config_pda,
+                merkle_root_upload_config: merkle_root_upload_config_pda,
+                authority: self.keypair.pubkey(),
+                system_program: system_program::ID,
+            }
+            .to_account_metas(None),
+        };
+
+        let blockhash = self.client.get_latest_blockhash()?;
+        let tx = Transaction::new_signed_with_payer(
+            &[ix],
+            Some(&self.keypair.pubkey()),
+            &[self.keypair.clone()],
+            blockhash,
+        );
+
+        self.client.send_transaction(&tx)?;
+
+        Ok(())
+    }
+
+    pub fn migrate_merkle_root_upload_config_authority(
+        &self,
+        vote_account: Pubkey,
+        epoch: u64,
+    ) -> anyhow::Result<()> {
+        let (tip_distribution_pda, _tip_distribution_bump) =
+            derive_tip_distribution_account_address(&self.program_id, &vote_account, epoch);
+        let (merkle_root_upload_config_pda, _merkle_root_upload_config_bump) =
+            derive_merkle_root_upload_config_account_address(&self.program_id);
+
+        let ix = Instruction {
+            program_id: self.program_id,
+            data: jito_tip_distribution_legacy::instruction::MigrateTdaMerkleRootUploadAuthority
+                .data(),
+            accounts: jito_tip_distribution_legacy::accounts::MigrateTdaMerkleRootUploadAuthority {
+                tip_distribution_account: tip_distribution_pda,
+                merkle_root_upload_config: merkle_root_upload_config_pda,
             }
             .to_account_metas(None),
         };
